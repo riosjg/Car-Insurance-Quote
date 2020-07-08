c4ed9a759f5dc51039ecd2d7cc5174b0b91e6ad7
Gextox <gabriel_il@live.com>
2020-07-08 20:43:22 -0300

    Updates

---
+++ precache-manifest.e88e58b10976bb049105638a8e0f27d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb49b02bb32eff4ec19fc8302ea1a5d",
+    "url": "/Car-Insurance-Quote/index.html"
+  },
+  {
+    "revision": "9c58b3557d40135322ae",
+    "url": "/Car-Insurance-Quote/static/css/main.58af0983.chunk.css"
+  },
+  {
+    "revision": "b37fcf9bd820cbd1c999",
+    "url": "/Car-Insurance-Quote/static/js/2.4ec6244f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Car-Insurance-Quote/static/js/2.4ec6244f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c58b3557d40135322ae",
+    "url": "/Car-Insurance-Quote/static/js/main.76ae8e1c.chunk.js"
+  },
+  {
+    "revision": "a70846e8b11d3009d08d",
+    "url": "/Car-Insurance-Quote/static/js/runtime-main.4f8b1a60.js"
+  }
+]);
